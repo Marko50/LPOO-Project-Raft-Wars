b08d749f57d5f56f1783e4146d5bc0be275bd8be
Marko50 <fernandoandre49@gmail.com>
2017-02-19 15:28:00 +0000

    "We could be heroes. #RoadTo20"

--- Iteration1#A/src/game.java
+++ Iteration1#A/src/game.java
@@ -16,9 +16,9 @@ public class game {
 		}
 	}
 
-	public static char[][] MazeCreator(int[] hPos, int[]gPos)
+	public static char[][] MazeCreator(char[][] res,int[] hPos, int[]gPos, boolean lever)
 	{
-		char[][] res = new char[10][10];
+		
 		
 		for(int i = 0; i < res.length; i++)
 		{
@@ -35,72 +35,209 @@ public class game {
 			res[res.length - 1][i] = 'X';
 		}
 		
-		char[] l1 = {'X', ' ', ' ', ' ', 'I', ' ', 'X', ' ' ,' ', 'X' };
-		char[] l2 = {'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ' ,' ', 'X' };
-		char[] l3 = {'X', ' ', 'I', ' ', 'I', ' ', 'X', ' ' ,' ', 'X' };
-		char[] l4 = {'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ' ,' ', 'X' };
-		char[] l5 = {'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', 'X' };
-		char[] l6 = {'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', 'X' };
-		char[] l7 = {'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X' ,' ', 'X' };
-		char[] l8 = {'X', ' ', 'I', ' ', 'I', ' ', 'X', 'K', ' ', 'X'};
-		
-		res[1] = l1;
-		res[2] = l2;
-		res[3] = l3;
-		res[4] = l4;
-		res[5] = l5;
-		res[6] = l6;
-		res[7] = l7;
-		res[8] = l8;
-		
-		res[hPos[0]][hPos[1]] = 'H';
-		res[gPos[0]][gPos[1]] = 'G';
+		if(lever)
+		{
+			char[] l1 = {'X', ' ', ' ', ' ', 'S', ' ', 'X', ' ' ,' ', 'X' };
+			char[] l2 = {'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ' ,' ', 'X' };
+			char[] l3 = {'X', ' ', 'S', ' ', 'S', ' ', 'X', ' ' ,' ', 'X' };
+			char[] l4 = {'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ' ,' ', 'X' };
+			char[] l5 = {'S', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', 'X' };
+			char[] l6 = {'S', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', 'X' };
+			char[] l7 = {'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X' ,' ', 'X' };
+			char[] l8 = {'X', ' ', 'S', ' ', 'S', ' ', 'X', 'K', ' ', 'X'};
+			
+			res[1] = l1;
+			res[2] = l2;
+			res[3] = l3;
+			res[4] = l4;
+			res[5] = l5;
+			res[6] = l6;
+			res[7] = l7;
+			res[8] = l8;
+		}
+		else
+		{
+			char[] l1 = {'X', ' ', ' ', ' ', 'I', ' ', 'X', ' ' ,' ', 'X' };
+			char[] l2 = {'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ' ,' ', 'X' };
+			char[] l3 = {'X', ' ', 'I', ' ', 'I', ' ', 'X', ' ' ,' ', 'X' };
+			char[] l4 = {'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ' ,' ', 'X' };
+			char[] l5 = {'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', 'X' };
+			char[] l6 = {'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ' ,' ', 'X' };
+			char[] l7 = {'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X' ,' ', 'X' };
+			char[] l8 = {'X', ' ', 'I', ' ', 'I', ' ', 'X', 'K', ' ', 'X'};
+			
+			res[1] = l1;
+			res[2] = l2;
+			res[3] = l3;
+			res[4] = l4;
+			res[5] = l5;
+			res[6] = l6;
+			res[7] = l7;
+			res[8] = l8;
+		}
+				
+		res[hPos[1]][hPos[0]] = 'H';
+		res[gPos[1]][gPos[0]] = 'G';
 		
 		return res;
 		
 	}
 	
-	public static void ChangeMaze(char[][] maze)
+	public static void environment()
 	{
 		int[] hPos = {1,1};
-		int[] gPos = {1,8};
-		
+		int[] gPos = {8,1};
+		char[][] maze = new char[10][10];
 		boolean flagVictory = false;
 		boolean flagDefeat = false;
 		boolean lever = false;
 		Scanner s = new Scanner(System.in);
-		String order;
+		char order;
 		while(!flagVictory && !flagDefeat)
 		{
-			System.out.println("Your move. Choose wisely");
-			showMaze(MazeCreator(hPos, gPos));
+			showMaze(MazeCreator(maze, hPos, gPos, lever));
 			while(true)
 			{
-				order = s.next();
-				if(order == "w")
+				System.out.println("Your move. Choose wisely");
+				order = s.next().charAt(0);
+				if(order == 'w')
 				{
-					if(maze[hPos[0]][hPos[1]-1] == 'W')
+					System.out.println(order);
+					if(maze[hPos[1]-1][hPos[0]] == 'X')
 					{
 						System.out.println("You triggered feminist.. Only men can do that");
 						continue;
 					}
-					else if(maze[hPos[0]][hPos[1]-1] == 'I' && !lever )
+					else if(maze[hPos[1]-1][hPos[0]] == 'I')
 					{
 						System.out.println("You shall not pass!");
 						continue;
 					}
+					else if(maze[hPos[1]-1][hPos[0]] == 'S')
+					{
+						System.out.println("You opened the door. Congratz. ");
+						hPos[1] = hPos[1] - 1;
+						flagVictory = true;
+						break;
+					}
+					
+					else if(maze[hPos[1]-1][hPos[0]] == 'K')
+					{
+						System.out.println("You pulled a mysterious lever. Jesus Christ superstar..");
+						lever = true;
+						break;
+					}
+					else
+					{
+						System.out.println("Wow fascinating...You just moved..");
+						hPos[1] = hPos[1] - 1;
+						break;
+					}
 					
 				}
-				else if(order == "a")
+				else if(order == 'a')
 				{
+					System.out.println(order);
+					if(maze[hPos[1]][hPos[0]-1] == 'X')
+					{
+						System.out.println("You triggered feminist.. Only men can do that");
+						continue;
+					}
+					else if(maze[hPos[1]][hPos[0]-1] == 'I' && !lever)
+					{
+						System.out.println("You shall not pass!");
+						continue;
+					}
+					else if(maze[hPos[1]][hPos[0]-1] == 'S')
+					{
+						System.out.println("You opened the door. Congratz. ");
+						hPos[0] = hPos[0] - 1;
+						flagVictory = true;
+						break;
+					}
+					
+					else if(maze[hPos[1]][hPos[0]-1] == 'K')
+					{
+						System.out.println("You pulled a mysterious lever. Jesus Christ superstar..");
+						lever = true;
+						break;
+					}
+					else
+					{
+						System.out.println("Wow fascinating...You just moved..");
+						hPos[0] = hPos[0] - 1;
+						break;
+					}
 					
 				}
-				else if(order == "s")
+				else if(order == 's')
 				{
+					System.out.println(order);
+					if(maze[hPos[1]+1][hPos[0]] == 'X')
+					{
+						System.out.println("You triggered feminist.. Only men can do that");
+						continue;
+					}
+					else if(maze[hPos[1]+1][hPos[0]] == 'I' && !lever)
+					{
+						System.out.println("You shall not pass!");
+						continue;
+					}
+					else if(maze[hPos[1]+1][hPos[0]] == 'S')
+					{
+						System.out.println("You opened the door. Congratz. ");
+						hPos[1] = hPos[1] + 1;
+						flagVictory = true;
+						break;
+					}
+					
+					else if(maze[hPos[1]+1][hPos[0]] == 'K')
+					{
+						System.out.println("You pulled a mysterious lever. Jesus Christ superstar..");
+						lever = true;
+						break;
+					}
+					else
+					{
+						System.out.println("Wow fascinating...You just moved..");
+						hPos[1] = hPos[1] + 1;
+						break;
+					}
 					
 				}
-				else if(order == "d")
+				else if(order == 'd')
 				{
+					System.out.println(order);
+					if(maze[hPos[1]][hPos[0]+1] == 'X')
+					{
+						System.out.println("You triggered feminist.. Only men can do that");
+						continue;
+					}
+					else if(maze[hPos[1]][hPos[0]+1] == 'I' && !lever)
+					{
+						System.out.println("You shall not pass!");
+						continue;
+					}
+					else if(maze[hPos[1]][hPos[0]+1] == 'S')
+					{
+						System.out.println("You opened the door. Congratz. ");
+						hPos[0] = hPos[0] + 1;
+						flagVictory = true;
+						break;
+					}
+					
+					else if(maze[hPos[1]][hPos[0]+1] == 'K')
+					{
+						System.out.println("You pulled a mysterious lever. Jesus Christ superstar..");
+						lever = true;
+						break;
+					}
+					else
+					{
+						System.out.println("Wow fascinating...You just moved..");
+						hPos[0] = hPos[0] + 1;
+						break;
+					}
 					
 				}
 			}
@@ -114,9 +251,7 @@ public class game {
 	
 	public static void main(String[] args) {
 		System.out.println("Olá Universo");
-		
-	
-		
+		environment();		
 	}
 
 }
